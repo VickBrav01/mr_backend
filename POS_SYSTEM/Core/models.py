34ccceb0544139e13b61f711927421244b6552af
VickBrav01 <victortosh001@gmail.com>
2025-05-27 12:25:02 +0300

    Core business details

---
+++ POS_SYSTEM/Core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class BusinessDetails(models.Model):
+    name = models.CharField(max_length=255, unique=True, blank=True, null=True)
+    pin = models.CharField(max_length=10, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    logo = models.ImageField(upload_to="business_logos/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
