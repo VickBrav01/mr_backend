7454fd685a69103b6a844cea1f36103db709f658
VickBrav01 <vickbrav01@gmail.com>
2025-06-13 18:53:55 +0300

    Search, Filter, Ordering Logic

--- POS_SYSTEM/POS_SYSTEM/settings.py
+++ POS_SYSTEM/POS_SYSTEM/settings.py
@@ -6,17 +6,11 @@ import cloudinary.uploader
 import cloudinary.api
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
