32cf51a4555bbb848cd405020132a6f172a306f4
Simon Chanzu <chanzusimon6@gmail.com>
2025-06-14 21:07:55 +0300

    customer and delivery updates

--- POS_SYSTEM/Customers/views.py
+++ POS_SYSTEM/Customers/views.py
@@ -2,21 +2,13 @@ from rest_framework.generics import ListAPIView
 from .serializers import CustomerSerializer
 from Delivery.models import Delivery
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
-
 
 class ListCustomersView(ListAPIView):
     serializer_class = CustomerSerializer
     permission_classes = [IsAuthenticated]
     queryset = Delivery.objects.all()
-    filter_backends = [
-        DjangoFilterBackend,
-        filters.SearchFilter,
-        filters.OrderingFilter,
-    ]
-    filterset_fields = ["created_at"]
-    search_fields = ["parcel_id", "customer_name", "email"]
-    ordering_fields = ["created_at", "username"]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["parcel_id", "customer_name"]
+    ordering_fields = ["created_at", "customer_name"]
     ordering = ["-created_at"]
--- POS_SYSTEM/Payments/views.py
+++ POS_SYSTEM/Payments/views.py
@@ -3,19 +3,12 @@ from Delivery.models import Delivery
 from .serializer import PaymentSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
-
 
 class PaymentsListView(ListAPIView):
     serializer_class = PaymentSerializer
     permission_classes = [IsAuthenticated]
     queryset = Delivery.objects.all()
-    filter_backends = [
-        DjangoFilterBackend,
-        filters.SearchFilter,
-        filters.OrderingFilter,
-    ]
-    filterset_fields = ["created_at"]
-    search_fields = ["parcel_id", "customer_name", "email"]
-    ordering_fields = ["created_at", "username"]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["parcel_id", "customer_name"]
+    ordering_fields = ["created_at", "customer_name"]
     ordering = ["-created_at"]
